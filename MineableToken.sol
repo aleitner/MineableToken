b50a93ff07dc7ae43c5640b440eafab08d94e125
Alexander Leitner <leitnersalex@gmail.com>
2017-08-11 14:34:16 -0400

    Add zeppelin as a submodule

--- MineableToken.sol
+++ MineableToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin/contracts/token/ERC20Basic.sol';
-import 'zeppelin/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/token/ERC20Basic.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 /* Centralized Administrator */
 contract MineableToken is Ownable {
