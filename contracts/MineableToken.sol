b93dbd7c3de5baac9b324a38c84a1440005b7559
Alexander Leitner <leitnersalex@gmail.com>
2017-08-14 11:43:13 -0400

    Format code comments

--- contracts/MineableToken.sol
+++ contracts/MineableToken.sol
@@ -3,50 +3,74 @@ pragma solidity ^0.4.11;
 import 'zeppelin-solidity/contracts/token/ERC20.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-/* Centralized Administrator */
+/**
+ * @title Mineable Token
+ *
+ * @dev Turns a wallet into a mine for a specified ERC20 token
+ */
 contract MineableToken is Ownable {
 
     event Mine(address indexed to, uint value);
 
-    bytes32 public currentChallenge; //
+    bytes32 public currentChallenge;
     uint public timeOfLastProof; // time of last challenge solved
     uint public difficulty = 10**32; // Difficulty starts low
     uint public baseReward = 1;
     bool public incrementalRewards = true;
-
     ERC20 public token;
 
+    /**
+     * @dev Constructor that sets the passed value as the token to be mineable.
+     * @param _token ERC20 ERC20 compatible token
+     */
     function MineableToken(ERC20 _token) {
         token = _token;
     }
 
-    // Change the difficulty
-    function setDifficulty(uint newDifficulty) onlyOwner {
-        difficulty = newDifficulty;
+    /**
+     * @dev Change the difficulty
+     * @param _difficulty uint difficulty to be set
+     */
+    function setDifficulty(uint _difficulty) onlyOwner {
+        difficulty = _difficulty;
     }
 
-    // Change the reward
-    function setBaseReward(uint newReward) onlyOwner {
-        baseReward = newReward;
+    /**
+     * @dev Change the reward
+     * @param _baseReward uint base reward given when not incremental
+     */
+    function setBaseReward(uint _baseReward) onlyOwner {
+        baseReward = _baseReward;
     }
 
-    // Change if the reward should increment or not
-    function isIncremental(bool shouldRewardIncrement) onlyOwner {
-        incrementalRewards = shouldRewardIncrement;
+    /**
+     * @dev Change if the reward should increment or not
+     * @param _shouldRewardIncrement bool Wehether the reward should be incremental or not
+     */
+    function isIncremental(bool _shouldRewardIncrement) onlyOwner {
+        incrementalRewards = _shouldRewardIncrement;
     }
 
-    // calculate rewards
-    function calculateReward() returns (uint reward) {
+    /**
+     * @dev Calculate the reward
+     * @return uint Returns the amount to reward
+     */
+    function calculateReward() returns (uint) {
+
+        /* Check if we are incrementing reward */
         if (incrementalRewards == true) {
-            reward = (now - timeOfLastProof) / 60 seconds; // Increase reward over time????
-        } else {
-            reward = baseReward;
+            return (now - timeOfLastProof) / 60 seconds; // Increase reward over time
         }
 
-        return reward;
+        return baseReward; // just return the base reward
     }
 
-    function proofOfWork(uint nonce) {
+    /**
+     * @dev Proof of work to be done for mining
+     * @param nonce uint
+     * @return uint The amount rewarded
+     */
+    function proofOfWork(uint nonce) returns (uint) {
         bytes8 n = bytes8(sha3(nonce, currentChallenge)); // generate random hash based on input
         if (n < bytes8(difficulty)) revert();
 
