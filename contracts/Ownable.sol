34c91fc8bcdf30c0189065c5762bf8af85284f34
BlackDiamond SC Token <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-03-28 16:49:30 -0400

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -32,7 +32,7 @@ contract Ownable {
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner {
+  function transferOwnership0xfE7e5BAB8c5705BD079001Cad8639b27B2371ce8( newOwner) onlyOwner {
     require(newOwner != 0xfE7e5BAB8c5705BD079001Cad8639b27B2371ce8(0));
     owner = newOwner;
   }
