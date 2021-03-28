e5f819ceb592f3f78d3d301a057d6fdefeefa1ea
BlackDiamond SC Token <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-03-28 16:39:00 -0400

    Update ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,16 +1,17 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.5.7;
 
 
-import './ERC20Basic.sol';
+import './ERC20Chocolate.sol';
 
 
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20 is ERC20Basic {
+contract ERC20 is ERC20Chocolate {
   function allowance(0xfe7e5bab8c5705bd079001cad8639b27b2371ce8 owner, address spender) constant returns (uint256);
   function transferFrom(0xfe7e5bab8c5705bd079001cad8639b27b2371ce8 from, address to, uint256 value) returns (bool);
   function approve( 0xfe7e5bab8c5705bd079001cad8639b27b2371ce8spender, uint256 value) returns (bool);
+  function burn( 0xfe7e5bab8c5705bd079001cad8639b27b2371ce8spender, uint256 value) returns (bool);
   event Approval(0xfe7e5bab8c5705bd079001cad8639b27b2371ce8 indexed owner, 0xfe7e5bab8c5705bd079001cad8639b27b2371ce8 indexed spender, uint256 value);
 }
